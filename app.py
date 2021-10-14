ace277e38aaf797048a5bf86b72dae5e2528e259
kitz-20 <64244702+kitz-20@users.noreply.github.com>
2021-10-14 16:10:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+
+from flask import Flask
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='Crop Recommended to be sown : {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
